57c57364b7d93af430df5f44be2e356232283cda
tfpresentes <83709963+tfpresentes@users.noreply.github.com>
2021-05-06 09:09:23 -0300

    pagina de inauguracao

--- pages/inauguracao.js
+++ pages/inauguracao.js
@@ -1,41 +1,9 @@
 function Home() {
   return (
-    <head>
-    <link rel="stylesheet" type="text/css" href="css/app.css">
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <meta name="viewport" content="width=device-width">
-    <title>My Basic Email Template Subject</title>
-    <!-- <style> -->
-  </head>
-  <body>
-    <span class="preheader"></span>
-    <table class="body">
-      <tr>
-        <td class="center" align="center" valign="top">
-          <center>
-            
-            <table align="center" class="wrapper header float-center"><tbody><tr><td class="wrapper-inner">
-              <table align="center" class="container"><tbody><tr><td>
-                <table class="row collapse"><tbody><tr>
-                  <th class="small-6 large-6 columns first"><table><tbody><tr><th>
-                    <img src="https://github.com/tfpresentes/landing-page/raw/main/img/logoTF-emailApresentacao.png">
-                  </th></tr></tbody></table></th>
-                  <th class="small-5 large-5 columns last"><table><tbody><tr><th>
-                    <img src="https://github.com/tfpresentes/landing-page/raw/main/img/ishopee-png.png">
-                  </th></tr></tbody></table></th>
-                </tr></tbody></table>
-              </td></tr></tbody></table>
-            </td></tr></tbody></table>
-            
-            <table align="center" class="container float-center"><tbody><tr><td>
-            
-              <table class="spacer"><tbody><tr><td height="16" style="font-size:16px;line-height:16px;">&nbsp;</td></tr></tbody></table> 
-              
-              <table class="row"><tbody><tr>
-                <th class="small-12 large-12 columns first last"><table><tbody><tr><th>
-                  
-                  <h1>Prezados vendedores, tudo bem?</h1>
-                  <p class="lead">Me chamo Gabriel, cumpro destacar que reuni neste e-mail todos os interessados em nosso serviço de ERP.</p>
+    <img src="https://github.com/tfpresentes/landing-page/raw/main/img/logoTF-emailApresentacao.png">
+    <img src="https://github.com/tfpresentes/landing-page/raw/main/img/ishopee-png.png">
+    <h1>Prezados vendedores, tudo bem?</h1>
+                  <p>Me chamo Gabriel, cumpro destacar que reuni neste e-mail todos os interessados em nosso serviço de ERP.</p>
                   <p>Para isso, farei uma breve apresentação da plataforma e responderei algumas dúvidas pertinentes nesse primeiro contato. Após apresentação aos que tiverem real interesse no serviço de gestão com estoque à pronta entrega poderemos marcar uma reunião virtual para demais esclarecimentos e possivelmente fecharmos negócio.</p>
                   <h2>Antes de mais nada...</h2>
                   <h2>QUEM SOMOS? E O QUE É UM ERP?</h2>
@@ -43,50 +11,19 @@ function Home() {
                   <p>Somos uma empresa de origem chinesa que trouxe para o Brasil uma inovação para auxiliar vendedores de pequeno a grande porte nos mais diversos marketplaces e lojas físicas.  Oferecemos toda automação de sistema de comunicação estoque - loja - fornecedor com acompanhamento de mercadoria, rastreamento, entrada e saída e fluxogramas e, AINDA, COM O BENEFÍCIO DO USO DO APLICATIVO OFERECEMOS NOSSAS MERCADORIAS À PRONTA ENTREGA PARA SUA REVENDA. NÃO TENHA ESTOQUE, oferecemos acesso à nosso estoque que conta com diversos produtos, desde tecnologia até roupas para venda diretamente na sua loja e mercadorias à pronta entrega atacado ou varejo que enviamos diretamente para seu consumidor final, tudo isso com apenas alguns cliques através do TF ERP - iShopee.</p>
                   <p>Neste mailing encontram-se contatos selecionados para participar de um período de teste grátis do sistema, ALÉM DE PARTICIPAREM DE UM SORTEIO NO QUAL OFERECEREMOS À 2 USUÁRIOS UM ACESSO GRÁTIS ESTENDIDO para nos ajudar a implementar correções iniciais do sistema.</p>
                   <p>Estamos integrando no momento os módulos de pagamento direto no site, até lá iremos tratar de valores via os contatos principais, whatsapp ou e-mail.</p>
-                  <table class="callout"><tbody><tr><th class="callout-inner primary">
-                    <p class="lead">Eu preciso comprar mercadorias específicas?</p>
+                  <p>Eu preciso comprar mercadorias específicas?</p>
                     <p> Não, você aloca o valor desejado para mercadorias e, enquanto houver saldo, qualquer mercadoria estará disponível para você.</p>
-                    <p class="lead">O serviço de ERP é pago?</p>
+                    <p>O serviço de ERP é pago?</p>
                     <p>Oferecemos 15 dias grátis para novos usuários interessados em explorar, se adaptar e lucrar com o software. Além desse período teste oferecemos planos pagos sob demanda, disponíveis para consulta em nosso site.</p>
                     <p>Oferecemos vários planos com diversas funcionalidades, embora nosso plano básico VIP 1 seja o suficiente para pequenos vendedores.</p>
-                  </th><th class="expander"></th></tr></tbody></table>
-                  <p class="lead">Esperamos encontrar você em breve!</p>
+                  <p>Esperamos encontrar você em breve!</p>
                   <p>Temos como diferencial, além do sistema de automação da sua loja em que permitirá que você trabalha em casa à frente de um computador, nós oferecemos à nossos cliente a comodidade de desfrutarem de nosso estoque com mais de 5.000 produtos e modelos catalogados, sem precisar ter dores de cabeça com embalagens, envio e etc.</p>
                   <p>Para mais informações sobre sistema e produtos disponíveis, agende uma reunião introdutória conosco, sem compromisso.</p>
                   <p>Atendimento personalizado, direto e exclusivo com os usuários através de Whatsapp e e-mail.</p>
-                </th>
-<th class="expander"></th></tr></tbody></table></th>
-              </tr></tbody></table>
-              <table class="wrapper secondary" align="center"><tbody><tr><td class="wrapper-inner">
-            
-                <table class="spacer"><tbody><tr><td height="16" style="font-size:16px;line-height:16px;">&nbsp;</td></tr></tbody></table> 
-            
-                <table class="row"><tbody><tr>
-                  <th class="small-12 large-6 columns first"><table><tbody><tr><th>
-                    <h5>Entre em contato:</h5>
-                    <table class="button facebook expand"><tbody><tr><td><table><tbody><tr><td><center><a href="http://zurb.com" align="center" class="float-center">Facebook</a></center></td></tr></tbody></table></td>
-<td class="expander"></td></tr></tbody></table>
-                    <table class="button twitter expand"><tbody><tr><td><table><tbody><tr><td><center><a href="http://zurb.com" align="center" class="float-center">Twitter</a></center></td></tr></tbody></table></td>
-<td class="expander"></td></tr></tbody></table>
-                    <table class="button google expand"><tbody><tr><td><table><tbody><tr><td><center><a href="http://zurb.com" align="center" class="float-center">Google+</a></center></td></tr></tbody></table></td>
-<td class="expander"></td></tr></tbody></table>
-                  </th></tr></tbody></table></th>
-                  <th class="small-12 large-6 columns last"><table><tbody><tr><th>
-                    <h5>Informações de contato:</h5>
+                <h5>Entre em contato:</h5>
+                <h5>Informações de contato:</h5>
                     <p>Phone: 408-341-0600</p>
                     <p>Email: <a href="mailto:foundation@zurb.com">foundation@zurb.com</a></p>
-                  </th></tr></tbody></table></th>
-                </tr></tbody></table>
-              </td></tr></tbody></table>
-            </td></tr></tbody></table>
-            
-          </center>
-        </td>
-      </tr>
-    </table>
-    <!-- prevent Gmail on iOS font size manipulation -->
-   <div style="display:none; white-space:nowrap; font:15px courier; line-height:0;"> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; </div>
-  </body>
   )
   
 }
