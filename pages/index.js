8aba0843110c0ee7f71826882cddc05e28dcadb7
tfpresentes <83709963+tfpresentes@users.noreply.github.com>
2021-05-13 16:25:44 -0300

    blank page only logo

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <img src="https://github.com/tfpresentes/landing-page/raw/main/img/email-mkt-01/logo-composto-slogan.png" width="300" height="" alt="" style="max-width: 100%; height: auto; border: 0; line-height: 100%; outline: 0; -ms-interpolation-mode: bicubic; font-size: 14px; color: #ffffff;">
+    return <div>iShopee!!!!</div>
 }
 
 export default Home
