956e90a2add38a7e25cbb7d6580f92ca096e880d
tfpresentes <83709963+tfpresentes@users.noreply.github.com>
2021-05-14 11:27:18 -0300

    blank page only logo

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,8 @@
-import '../css/styles.css'
-
 function Home() {
     return (
         <div>
             <h1>iShopee!!!</h1>
-            <img src="https://github.com/tfpresentes/landing-page/raw/main/img/webMockup.png" />
+            <img src="https://github.com/tfpresentes/landing-page/raw/main/img/webMockup.png" style="max-width: 100%;"/>
         </div>
     )
 }
