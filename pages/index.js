2895601e3b34d74e627bb590b35ef33ff621d41e
tfpresentes <83709963+tfpresentes@users.noreply.github.com>
2021-05-29 08:53:10 -0300

    blank page only logo

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,17 @@
-// var app = new Vue({
-//     el:'app',
-//     data: {
-//         text: 'iShopeee!!!'
-//     }
-// });
+import Image from 'next/image'
+
+function Home() {
+    return (
+        <div>
+            <h1>iShopee!!!</h1>
+            <image
+                src="https://github.com/tfpresentes/landing-page/raw/main/img/webMockup.png"
+                alt="webpage design"
+                width={500}
+                height={500}
+            />
+        </div>
+    )
+}
+
+export default Home
