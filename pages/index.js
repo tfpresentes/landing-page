3f4fd57f74112cd8378dac6647ce00ef50f0f36e
tfpresentes <83709963+tfpresentes@users.noreply.github.com>
2021-05-29 08:30:58 -0300

    blank page only logo

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-var app = new Vue({
-    el:'app',
-    data: {
-        text: 'iShopeee!!!'
-    }
-});
+// var app = new Vue({
+//     el:'app',
+//     data: {
+//         text: 'iShopeee!!!'
+//     }
+// });
